1154592c33a0da2f205bb6aaaa69de6cbb0457f5
Isaias da Silva <isaias.silva@itera.com.br>
2024-05-30 11:31:11 -0300

    firt commit

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+type rootCmd struct {
+	cmd *cobra.Command
+}
+
+func (r *rootCmd) execute() {
+	if err := r.cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func newRootCmd() *rootCmd {
+
+	root := &rootCmd{}
+	cmd := &cobra.Command{
+		Use:               "chdb",
+		Short:             "dump with go",
+		Long:              ``,
+		SilenceUsage:      true,
+		SilenceErrors:     true,
+		Args:              cobra.NoArgs,
+		ValidArgsFunction: cobra.NoFileCompletions,
+	}
+
+	cmd.AddCommand(
+		newCheckCmd().cmd,
+	)
+	root.cmd = cmd
+	return root
+}
+
+func Execute() { newRootCmd().execute() }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/idasilva/chdb-dump/cmd"
+
+func main() {
+	cmd.Execute()
+}
